aa601bf2351eaed93e766d7cd1245b41c44a3116
Balint Kleri <b@balintbrews.com>
2025-05-04 14:06:37 +0200

    build: package library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'use-trailing-async',
+      fileName: (format) => `use-trailing-async.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
   },
 });
